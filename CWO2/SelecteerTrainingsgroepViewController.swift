521370062dce0be30ea781f14512028104aab0a5
pim <contact@pimpraat.com>
2018-03-05 09:57:11 +0100

    added 20 sailors and first design improvements

---
+++ CWO2/SelecteerTrainingsgroepViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SelecteerTrainingsgroepViewController.swift
+//  CWO2
+//
+//  Created by Pim Praat on 03/03/2018.
+//  Copyright © 2018 Pim Praat. All rights reserved.
+//
+
+import UIKit
+
+class SelecteerTrainingsgroepViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
